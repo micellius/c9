823b1a968703fbc645b14f10fd5dc74b7dadfcd8
micellius <micellius@gmail.com>
2015-11-07 15:30:09 +0000

    core(.editorconfig) add editor config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jscs: {
-        src: "src/**/*.js",
-        options: {
-            config: ".jscsrc",
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jscs: {
+            src: "src/**/*.js",
+            options: {
+                config: ".jscsrc",
+            }
         }
-    }
-  });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-jscs');
-
-  // Default task(s).
-  grunt.registerTask('default', ['jscs']);
+    });
+    
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-jscs');
+    
+    // Default task(s).
+    grunt.registerTask('default', ['jscs']);
 
 };
